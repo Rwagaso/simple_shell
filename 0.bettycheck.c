b246637bacbf64e4e88e0ce7644a77db3dca8c35
Sethuslt <temaswati06@gmail.com>
2023-05-13 21:48:15 -0700

    BETTY would be proud

---
+++ 0.bettycheck.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - writing a code that betty will check as okay
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("checking if betty will approve , \n");
+	return (0);
+}
