3e09462bd8599c318fffa61a99c6b3c3be2e9c10
Sethuslt <temaswati06@gmail.com>
2023-05-27 03:48:06 -0700

    more of shell

--- main.c
+++ main.c
@@ -16,9 +16,11 @@ int main(int ac, char **av)
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			: "=r" (fd));
-	while(ac == 2
-i{
+			"add $3, %0"
+			:"=r" (fd)
+			: "r" (fd));
+	while(ac == 2)
+{
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
@@ -43,7 +45,7 @@ i{
 	read_history(info);
 	hsh(info,av);
 
-	return (EXIT_SUCCESS);
+return (EXIT_SUCCESS);
 
 }
 
